8c9ddd695822cfb6242bef16b17741b538c20635
Shivakant6387 <shivakant5875@gmail.com>
2023-01-05 14:22:25 +0530

    setter based dependency inject

--- src/main/java/org/example/collection/CollectionTest.java
+++ src/main/java/org/example/collection/CollectionTest.java
@@ -1,2 +1,16 @@
-package org.example.collection;public class CollectionTest {
+package org.example.collection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CollectionTest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        Collection jc=(Collection)context.getBean("collection");
+
+        jc.getAddressList();
+        jc.getAddressSet();
+        jc.getAddressMap();
+        jc.getAddressProp();
+    }
 }
--- src/main/java/org/example/com/student/EmpTest.java
+++ src/main/java/org/example/com/student/EmpTest.java
@@ -1,2 +1,16 @@
-package org.example.com.student;public class EmpTest {
+package org.example.com.student;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class EmpTest {
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("collectionconfig.xml");
+        Emp emp= (Emp) context.getBean("emp");
+        System.out.println(emp.getName());
+        System.out.println(emp);
+        System.out.println(emp.getAddresses());
+        System.out.println(emp.getPhones());
+        System.out.println(emp.getCourses());
+    }
 }
