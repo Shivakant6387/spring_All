bcdeadd1cca591ccd99f38cc269e2ee3f5b5deb3
Shivakant6387 <shivakant5875@gmail.com>
2023-01-10 13:29:20 +0530

    we are using singleton with Configuration

---
+++ src/main/java/org/example/singleton/MyBean.java
@@ -0,0 +1,8 @@
+package org.example.singleton;
+
+public class MyBean {
+    public MyBean() {
+        System.out.println("MyBean instance created");
+    }
+
+}
---
+++ src/main/java/org/example/singleton/MyConfiguration.java
@@ -0,0 +1,14 @@
+package org.example.singleton;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class MyConfiguration {
+    @Bean
+    @Scope(value="singleton")
+    public MyBean myBean() {
+        return new MyBean();
+    }
+}
